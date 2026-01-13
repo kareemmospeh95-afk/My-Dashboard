32c147c58b8e58440f0ec78109b7dd32fbaab578
kareemmospeh <kareemmospeh95@gmail.com>
2026-01-12 20:50:29 -0500

    javascript

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,17 @@
 
+let repoesNum = document.getElementById('length');
+
+getRep();
+//get repos function
+function getRep() {
+
+    fetch(`https://api.github.com/users/kareemmospeh95-afk/repos`)
+        .then((res) => res.json())
+        .then((repos) => {
+            repoesNum.innerHTML = `${repos.length}`
+            console.log(repos)
+        }
+        )
+}
+
 
